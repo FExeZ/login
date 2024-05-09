3f60e827750f1bd55f69bfbdcc6c6373404b8921
Franco <francoexequielzerda@gmail.com>
2024-05-09 15:51:38 -0300

    creaciones de las controladoras de Persistencia

---
+++ src/main/java/com/mycompany/login/logica/Controladora.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.login.logica;
+
+import com.mycompany.login.persistencia.ControladoraPersistencia;
+
+/**
+ *
+ * @author Marisa
+ */
+public class Controladora {
+    
+    ControladoraPersistencia controlPersis = new ControladoraPersistencia();
+    
+}
--- src/main/java/com/mycompany/login/persistencia/UsuarioJpaController.java
+++ src/main/java/com/mycompany/login/persistencia/UsuarioJpaController.java
@@ -12,6 +12,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.persistence.EntityNotFoundException;
+import javax.persistence.Persistence;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
@@ -26,6 +27,10 @@ public class UsuarioJpaController implements Serializable {
         this.emf = emf;
     }
     private EntityManagerFactory emf = null;
+    
+    public UsuarioJpaController(){
+        emf = Persistence.createEntityManagerFactory("loginPU");
+    }
     
     public EntityManager getEntityManager(){
         return emf.createEntityManager();
